c7b5d8ddeb2d17fbf29b95b7eb249c5a6bafbf1b
JLoayza <loayza.juan@pucp.pe>
2016-06-04 22:35:03 -0500

    migrates mjjj

---
+++ database/migrations/2016_06_04_194431_create_familiarxsocio_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFamiliarxsocioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('familiarxsocio', function (Blueprint $table) {
+            $table->integeger('socio_id')->unsigned()->nullable();
+            $table->integer('persona_id')->unsigned()->nullable();
+            $table->integer('tipo_relacion_id')->unsigned()->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('familiarxsocio');
+    }
+}
