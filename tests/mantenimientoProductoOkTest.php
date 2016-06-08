b1810c9531d16a3cf786828252ea133b6568bf65
Joe Natan <a20105726@pucp.pe>
2016-06-08 18:04:06 -0500

    Test mantenimiento Producto

---
+++ tests/mantenimientoProductoOkTest.php
@@ -0,0 +1,32 @@
+<?php
+class Example extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*chrome");
+    $this->setBrowserUrl("http://200.16.7.112/");
+  }
+
+  public function testMyTestCase()
+  {
+    $this->click("link=PRODUCTOS");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=Agregar Producto");
+    $this->waitForPageToLoad("30000");
+    $this->type("id=nombre", "Producto1");
+    $this->type("id=descripcion", "producto firma papus");
+    $this->select("id=id_tipo_producto", "label=Souvenirs");
+    $this->click("css=input.btn.btn-success");
+    $this->waitForPageToLoad("30000");
+    $this->click("css=#modalSuccess > div.modal-dialog > div.modal-content > div.modal-footer > button.btn.btn-default");
+    $this->click("link=LOGIN");
+    $this->waitForPageToLoad("30000");
+    $this->type("name=email", "adming@mail.com");
+    $this->type("name=password", "123456");
+    $this->click("//button[@type='submit']");
+    $this->waitForPageToLoad("30000");
+    $this->click("link=Regresar");
+    $this->waitForPageToLoad("30000");
+  }
+}
+?>
