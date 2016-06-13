d25f3402bed1e040506aa85a30dcfa94b03537c6
CESAR OLIVERA <CESAR OLIVERA>
2016-06-13 12:24:15 -0500

    Corrección de error de sintaxis en la semilla de postulantes (faltaba una coma)

--- database/seeds/PostulanteTableSeeder.php
+++ database/seeds/PostulanteTableSeeder.php
@@ -28,7 +28,8 @@ class PostulanteTableSeeder extends Seeder
         	'nro_hijos'=>20,
         	'domicilio'=>'La perla',
         	'telefono_domicilio'=>84516235,
-        	'telefono_celular'=>989456123]);
+        	'telefono_celular'=>989456123
+        ]);
 
         Postulante::create([
         	'id_postulante'=>2,
@@ -46,7 +47,8 @@ class PostulanteTableSeeder extends Seeder
         	'nro_hijos'=>20,
         	'domicilio'=>'La perla',
         	'telefono_domicilio'=>84516235,
-        	'telefono_celular'=>989456123]);
+        	'telefono_celular'=>989456123
+        ]);
 
         Postulante::create([
             'id_postulante'=>5,
@@ -66,7 +68,8 @@ class PostulanteTableSeeder extends Seeder
             'estado_civil'=>'soltero',
             'nro_hijos'=>1,
             'domicilio'=>'Av. Las torres 515',
-            'telefono_domicilio'=>3265025
-            'telefono_celular'=>992745845]);
+            'telefono_domicilio'=>3265025,
+            'telefono_celular'=>992745845
+        ]);
     }
 }
