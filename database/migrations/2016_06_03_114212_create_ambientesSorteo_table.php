2e3d55281e652b77efe548dca2c81d49d24cd11e
maxvg <christian.vilcapomag@pucp.pe>
2016-06-15 18:50:38 -0500

    cambio de nombre de tabla en mayuscula

--- database/migrations/2016_06_03_114212_create_ambientesSorteo_table.php
+++ database/migrations/2016_06_03_114212_create_ambientesSorteo_table.php
@@ -12,7 +12,7 @@ class CreateAmbientesSorteoTable extends Migration
      */
     public function up()
     {
-        Schema::create('ambientesSorteo',function (Blueprint $table){
+        Schema::create('ambientessorteo',function (Blueprint $table){
             //Estos son los atributos que estaban en el caso de uso
             $table->integer('id');
             $table->integer('id_ambiente');      
