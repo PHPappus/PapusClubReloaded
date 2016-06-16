9b9bf6d121d1f7703dce5e6edd8c7ca404b7afe0
maxvg <christian.vilcapomag@pucp.pe>
2016-06-16 08:55:49 -0500

    cambios funciona

--- database/migrations/2016_06_03_114212_create_ambientesSorteo_table.php
+++ database/migrations/2016_06_03_114212_create_ambientesSorteo_table.php
@@ -14,11 +14,11 @@ class CreateAmbientesSorteoTable extends Migration
     {
         Schema::create('ambientessorteo',function (Blueprint $table){
             //Estos son los atributos que estaban en el caso de uso
-            $table->integer('id_sorteo')->unsigned();
+            $table->integer('id')->unsigned();
             $table->integer('id_ambiente')->unsigned();            
             $table->softDeletes();
             $table->timestamps();
-            $table->primary(['id_sorteo', 'id_ambiente']);
+            //$table->primary(['id_sorteo', 'id_ambiente']);
         });
     }
 
