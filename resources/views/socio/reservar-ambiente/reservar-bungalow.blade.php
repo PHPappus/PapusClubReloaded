a13fcede7db859f59551ec6b108588a24e2a9fe3
JDenisse <jd.alvag@gmail.com>
2016-06-16 00:41:41 -0500

    vista routear

--- resources/views/socio/reservar-ambiente/reservar-bungalow.blade.php
+++ resources/views/socio/reservar-ambiente/reservar-bungalow.blade.php
@@ -45,7 +45,7 @@
 	<br/>
 
 	<div class="container">
-		<form method="POST" class="form-horizontal form-border" action="/reservar-ambiente/reservar-bungalow/search">
+		<form method="POST" class="form-horizontal form-border" action="/reservar-ambiente/reservar-bungalow/search-adminR">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
 			<br/>
 			<div class="form-group">
