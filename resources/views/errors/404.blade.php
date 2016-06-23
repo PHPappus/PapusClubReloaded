d6a8c147c5dd5585158e84767a61faff75118da8
Joe Natan <a20105726@pucp.pe>
2016-06-22 21:19:04 -0500

    pantalla error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,53 @@
+  <!doctype html> 
+  <html class="no-js" lang="en-US"> 
+  	<head> 
+  		
+
+
+		<title>Home Page</title>
+		<meta charset="UTF-8">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/jquery.bxslider.css">
+		<link rel="stylesheet" href="css/font-awesome.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/MisEstilos.css">
+		<!-- <link rel="stylesheet" type="text/css" href="css/estilos.css"> -->
+			<link rel='stylesheet' id='' href='http://grainandmortar.com/wp-content/themes/grain-and-mortar/assets/css/main.css?v=1.13' type='text/css' media='all'/>
+			<link rel='stylesheet' id='grainandmortar_compatibility-css' href='http://i56wybt9jm-flywheel.netdna-ssl.com/wp-content/themes/grain-and-mortar/assets/css/compatibility.css?v=1.12' type='text/css' media='all'/><link rel='https://api.w.org/' href='http://grainandmortar.com/wp-json/'/> 
+			<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style> 
+</head> 
+
+
+				<body class="error404 content-wrapper"> 
+		 		<div id="loader-bar"><dd></dd><dt></dt></div>
+		 		<div id="main" role="document"> <main class="main" role="main" data-theclass="error404 content-wrapper"> <article class="full"> 
+		 			<section class="container bleed"> 
+		 			<div class="column"> 
+		 			<div class="content kitchensink"> <img src="http://i56wybt9jm-flywheel.netdna-ssl.com/wp-content/themes/grain-and-mortar/assets/img/404-tiger.png" class="tiger_face"/> <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/" x="0px" y="0px" width="471px" height="259.4px" viewBox="0 0 471 259.4" class="error404-letters" style="enable-background:new 0 0 471 259.4;" xml:space="preserve"> <style type="text/css"> .error404-st6{fill:#FFFFFF;} </style> <defs> </defs> <g class="anim fadeIn left delay-100"> <path class="error404-st6" stroke="#fff" stroke-width="1.25px" d="M0,175.7L79.3,51.6h7v124.5c4.7-0.1,8.4-0.5,11.1-1c2.8-0.5,4.9-1.5,6.3-2.9c1.4-1.4,2.4-3.5,2.8-6.1c0.4-2.6,0.7-6.1,0.7-10.5h7.9v50.2h-7.9c0-4.1-0.2-7.4-0.7-9.9c-0.5-2.6-1.4-4.5-2.8-6c-1.4-1.4-3.5-2.4-6.3-2.9c-2.8-0.5-6.5-0.9-11.1-1v4.7c0,5.9,0.2,10.6,0.7,14.2c0.5,3.6,1.3,6.4,2.6,8.4c1.3,2,3.1,3.4,5.4,4.2c2.3,0.8,5.4,1.1,9.1,1.1v8.6H40.8v-8.6c5,0,9-0.4,12-1.1c3.1-0.8,5.4-2.1,7.1-4.1c1.6-1.9,2.8-4.7,3.4-8.1c0.6-3.5,0.9-7.8,0.9-13.1V187H0V175.7z M64.2,95.6h-1.1l-51.8,80.2h52.9V95.6z"/> </g> <g class="anim fadeIn right delay-100"> <path class="error404-st6" stroke="#fff" stroke-width="1.25px" d="M355.9,175.7l79.3-124.1h7v124.5c4.7-0.1,8.4-0.5,11.1-1c2.8-0.5,4.9-1.5,6.3-2.9c1.4-1.4,2.4-3.5,2.8-6.1c0.4-2.6,0.7-6.1,0.7-10.5h7.9v50.2h-7.9c0-4.1-0.2-7.4-0.7-9.9c-0.5-2.6-1.4-4.5-2.8-6c-1.4-1.4-3.5-2.4-6.3-2.9c-2.8-0.5-6.5-0.9-11.1-1v4.7c0,5.9,0.2,10.6,0.7,14.2c0.5,3.6,1.3,6.4,2.6,8.4c1.3,2,3.1,3.4,5.4,4.2c2.3,0.8,5.4,1.1,9.1,1.1v8.6h-63.3v-8.6c5,0,9-0.4,12-1.1c3.1-0.8,5.4-2.1,7.1-4.1c1.6-1.9,2.8-4.7,3.4-8.1c0.6-3.5,0.9-7.8,0.9-13.1V187h-64.2V175.7zM420.1,95.6H419l-51.8,80.2h52.9V95.6z"/> </g> </svg> <p class="text-center description">¡Hola Tigre! Ha querido acceder a una área que no existe</p> <p class="the_linkage"> <a href="/" class="button">Click para regresar a casa</a> <span class="tonight">¡Papu!</span> </p> </div> </div></section></article> </main> </div>  <div id="work-modal" class="modal-container anim"> <div class="modal-off-canvas"></div> <div class="modal-wrapper"> <div class="modal"> <section id="work-modal-head" class="page-hero"> <div class="container bleed"> <div class="column seven"> <div class="content"> <h1 class="h1"><em>Filter Work</em></h1> <div class="vertical-split"></div> <div class="description"><p class="supporting-copy">Click the topics that interest you most.</p></div> </div> </div> <div class="column five"> <div class="content"> <a href="#close" class="close caf" data-func="close_modal">&times;</a> </div> </div> </div> </section> <section id="add-items"> <form class="container bleed one-third"> <div class="column active"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering_all" name="filtering[]" value="all" checked tabindex="-1"/><div class="outer"><div class="inner"></div></div></div>All</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="animation" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Animation</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="branding" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Branding</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="conference-event" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Conference/Event</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="content" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Content</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="environmental-design" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Environmental Design</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="food-and-beverage" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Food &amp; Beverage</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="hardware" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Hardware Product</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="illustration" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Illustration</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="print-and-packaging" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Print &amp; Packaging</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="product-launch" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Product Launch</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="software-as-a-service" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Software as a Service</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="software-product-design" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Software Product Design</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="startup" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Startup</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="strategy" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Strategy</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="ui-ux" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>UI/UX</label></div> <div class="column"><label tabindex="0"><div class="checkbox"><input type="checkbox" id="filtering" name="filtering[]" value="website-design-and-development" tabindex="-1" /><div class="outer"><div class="inner"></div></div></div>Website Design &amp; Development</label></div> </form> <div><p>&nbsp;</p><p class="text-center"><a href="/work" class="the_filter_link ajax button">update filters</a></p></div> </section> </div> </div></div> <div id="center"></div> <div id="full"></div> <script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script><script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/detectizr/2.2.0/detectizr.min.js'></script><script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script><script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="http://i56wybt9jm-flywheel.netdna-ssl.com/wp-content/themes/grain-and-mortar/assets/js/min/jquery-min.js?2.1.4"><\/script>')</script><script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/jquery.pjax/1.9.6/jquery.pjax.min.js'></script><script type='text/javascript' src='http://i56wybt9jm-flywheel.netdna-ssl.com/wp-content/themes/grain-and-mortar/assets/js/min/element_queries-min.js'></script><script type='text/javascript' src='http://i56wybt9jm-flywheel.netdna-ssl.com/wp-content/themes/grain-and-mortar/assets/js/min/scripts-min.js?v=1.12'></script><script type='text/javascript' src='http://i56wybt9jm-flywheel.netdna-ssl.com/wp-content/themes/grain-and-mortar/assets/js/min/jquery.scrolldepth-min.js'></script><script type='text/javascript' src='http://i56wybt9jm-flywheel.netdna-ssl.com/wp-includes/js/wp-embed.min.js?ver=4.5.2'></script> <script type="text/javascript">
+	$('li:nth-of-type(2)','.dropdown-menu').hover(function() {
+		$(this).parent().parent().addClass('first-active')
+	}, function() {
+		$(this).parent().parent().removeClass('first-active')
+	});
+
+	$('a','.dropdown-menu').focus(function() {
+		$(this).parent().parent().addClass('nav-opened');
+	});
+
+	$('li:nth-of-type(2)','.dropdown-menu').focus(function() {
+		$(this).parent().parent().addClass('first-active')
+	});
+
+
+	$('a:last','.dropdown-menu').blur(function() {
+		$(this).parent().parent().removeClass('nav-opened');
+	});
+
+	$('li:nth-of-type(2)','.dropdown-menu').blur(function() {
+		$(this).parent().parent().removeClass('nav-opened');
+		$(this).parent().parent().removeClass('first-active')
+	});
+
+
+
