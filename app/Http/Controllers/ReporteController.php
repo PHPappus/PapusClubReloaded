c14929f03f70ead771fd09376ba9e23165748dbc
Zikokun <fmore91@gmail.com>
2016-06-25 17:15:27 -0500

    reporte de socios morosos completado

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -11,7 +11,9 @@ use papusclub\Models\Configuracion;
 use papusclub\Models\Persona;
 use papusclub\Models\Sede;
 use Auth;
+use Carbon\Carbon;
 use papusclub\User;
+use papusclub\Perfil;
 
 class ReporteController extends Controller
 {
@@ -32,10 +34,59 @@ class ReporteController extends Controller
         $sedes = Sede::all();   
         return view('gerente.reportes.reporte-morosos',compact('sedes'));
     }
-     public function reporte2Final() 
+     public function reporte2Final(Request $request) 
     {
         $sedes = Sede::all();
-        return view('gerente.reportes.reporte-morosos-final',compact('sedes'));
+        $input = $request->all();
+        $carbon=new Carbon;
+        //obtener el responsable del reporte
+        $perfilResponsable=Perfil::where('description','=','ADMINISTRADOR DE PAGOS')->first();
+        $responsable=User::where('perfil_id','=',$perfilResponsable->id)->get();
+        //fin obtener el responsable del reporte
+
+        
+        //obtener fechas
+        $fechaIni=new Carbon('America/Lima');
+        $fechaFin=new Carbon('America/Lima');
+        $fechaAct=new Carbon('America/Lima');
+        if(!empty($input['fecha_inicio'])){
+            $a_realizarse_en = str_replace('/', '-', $input['fecha_inicio']);
+            $fechaIni=$carbon->createFromFormat('d-m-Y', $a_realizarse_en);
+        }
+        if(!empty($input['fecha_fin'])){
+            $a_realizarse_en = str_replace('/', '-', $input['fecha_fin']);
+            $fechaFin=$carbon->createFromFormat('d-m-Y', $a_realizarse_en);
+        }
+        //fin obtener fechas
+
+        //obtener socios morosos
+        $socios=Socio::all();
+        $subtotales=array();
+        foreach ($socios as $key => $socio) {
+                $subtotal=0;
+                $facturaciones = $socio->postulante->persona->facturacion;
+                if(count($facturaciones)!=0){
+                    foreach ($facturaciones as $i => $facturacion) {
+                        if($facturacion->estado=='Emitido'){
+                            $subtotal=$subtotal+$facturacion->total;
+                        }
+
+                    }
+                }else{
+                    unset($socios[$key]);
+                }
+                if($subtotal==0){
+                    unset($socios[$key]);
+                }else{
+                    array_push($subtotales,$subtotal);
+                }
+        }
+        
+        $totalDeuda=array_sum($subtotales);
+        
+        $valores=array_reverse($subtotales);
+        //fin obtener socios morosos
+        return view('gerente.reportes.reporte-morosos-final',compact('sedes','responsable','fechaIni','fechaFin','fechaAct','socios','totalDeuda','valores'));
     }
     //Reporte 3:cuantas veces se reserva un bunalow en un rango de fecha 
     public function reporte3() 
