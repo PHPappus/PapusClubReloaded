51ad47f9fc9afa1b48fda75307ade76917692153
Victor Fuentes Ramos <Victor Fuentes Ramos>
2016-06-24 20:04:14 -0500

    Editar ambiente

--- app/Http/Controllers/ActividadController.php
+++ app/Http/Controllers/ActividadController.php
@@ -115,7 +115,7 @@ class ActividadController extends Controller
                 }else{
                     $a_realizarse_en = str_replace('/', '-', $input['a_realizarse_en']);      
                     $actividad->a_realizarse_en=$carbon->createFromFormat('Y-m-d', $a_realizarse_en)->toDateString();
-                    $actividad->hora_inicio=$carbon->createFromFormat('H:i', $input['hora'])->toTimeString();
+                    $actividad->hora_inicio=$carbon->createFromFormat('H:i:s', $input['hora'])->toTimeString();
         }
         
         $actividad->update();
