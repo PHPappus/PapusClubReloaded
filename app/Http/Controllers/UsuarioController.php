67222a4a3478a8a34e7b7ee9f124c4b5ecdbdd5d
juani <juan0094@gmail.com>
2016-07-06 00:21:27 -0500

    agregando try catchs

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -20,6 +20,7 @@ use Redirect;
 use Illuminate\Routing\Route;
 use Hash;
 use DB;
+use Log;
 
 class UsuarioController extends Controller
 {
@@ -31,64 +32,86 @@ class UsuarioController extends Controller
 
     public function cuenta()
     {
-        $perfil = 'socio';
-        switch (\Auth::user()->perfil_id) {
-                case '1':
-                    $perfil='socio';
-                    break;
-                case '2':
-                    $perfil='admin';
-                    break;
-                case '3':
-                    $perfil='admin-pagos';
-                    break;
-                case '4':
-                    $perfil='admin-registros';
-                    break;
-                case '5':
-                    $perfil='gerente';
-                    break;
-                case '6':
-                    $perfil='admin-persona';
-                    break;
-                case '7':
-                    $perfil='admin-reserva';
-                    break;
-                case '8':
-                    $perfil='control-ingresos';
-                    break;
-        }
-        if($perfil!='socio')
+
+        try
         {
-            return view('auth.cuenta', compact('perfil'));        
+            $perfil = 'socio';
+            switch (\Auth::user()->perfil_id) {
+                    case '1':
+                        $perfil='socio';
+                        break;
+                    case '2':
+                        $perfil='admin';
+                        break;
+                    case '3':
+                        $perfil='admin-pagos';
+                        break;
+                    case '4':
+                        $perfil='admin-registros';
+                        break;
+                    case '5':
+                        $perfil='gerente';
+                        break;
+                    case '6':
+                        $perfil='admin-persona';
+                        break;
+                    case '7':
+                        $perfil='admin-reserva';
+                        break;
+                    case '8':
+                        $perfil='control-ingresos';
+                        break;
+            }
+            if($perfil!='socio')
+            {
+                return view('auth.cuenta', compact('perfil'));        
 
+            }
+            else
+            {
+                    $user_id = Auth::user()->id;
+                    $usuario = User::find($user_id);
+                    $persona_id = $usuario->persona->id;               
+                    $postulante = Postulante::find($persona_id);
+                    $socio = Socio::where('postulante_id','=',$postulante->id_postulante)->first();                           
+                    $estado_civil=Configuracion::find($socio->postulante->estado_civil);
+                    $carbon=new Carbon();
+                    $socio->carnet_actual()->fecha_emision=$carbon->createFromFormat('Y-m-d',$socio->carnet_actual()->fecha_emision)->format('d/m/Y');
+                    $socio->carnet_actual()->fecha_vencimiento=$carbon->createFromFormat('Y-m-d',$socio->carnet_actual()->fecha_vencimiento)->format('d/m/Y');
+                    $socio->postulante->persona->fecha_nacimiento=$carbon->createFromFormat('Y-m-d',$socio->postulante->persona->fecha_nacimiento)->format('d/m/Y');                
+
+
+
+
+
+                return view('auth.cuentasocio',compact('socio','estado_civil'));
+            }           
         }
-        else
+        catch(\Exception $e)
         {
-                $user_id = Auth::user()->id;
-                $usuario = User::find($user_id);
-                $persona_id = $usuario->persona->id;               
-                $postulante = Postulante::find($persona_id);
-                $socio = Socio::where('postulante_id','=',$postulante->id_postulante)->first();                           
-                $estado_civil=Configuracion::find($socio->postulante->estado_civil);
-                $carbon=new Carbon();
-                $socio->carnet_actual()->fecha_emision=$carbon->createFromFormat('Y-m-d',$socio->carnet_actual()->fecha_emision)->format('d/m/Y');
-                $socio->carnet_actual()->fecha_vencimiento=$carbon->createFromFormat('Y-m-d',$socio->carnet_actual()->fecha_vencimiento)->format('d/m/Y');
-                $socio->postulante->persona->fecha_nacimiento=$carbon->createFromFormat('Y-m-d',$socio->postulante->persona->fecha_nacimiento)->format('d/m/Y');                
+            Log::error($e);
+            $error = 'UsuarioController-cuenta';
+            return view('errors.corrigeme', compact('error'));            
+        }        
 
-
-
-
-
-            return view('auth.cuentasocio',compact('socio','estado_civil'));
-        }
     }
 
     public function index()
     {
-        $users     = \papusclub\User::All();
-        $perfiles  = \papusclub\Perfil::All();
-        return view('usuario.consultar',compact('users'),compact('perfiles'));
+
+        try
+        {
+            $users     = \papusclub\User::All();
+            $perfiles  = \papusclub\Perfil::All();
+            return view('usuario.consultar',compact('users'),compact('perfiles'));           
+        }
+        catch(\Exception $e)
+        {
+            Log::error($e);
+            $error = 'UsuarioController-index';
+            return view('errors.corrigeme', compact('error'));            
+        }         
+
     }
 
     /**
@@ -97,12 +120,23 @@ class UsuarioController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create()
-    {   
-        $perfiles=Perfil::all();
-        $personas = Persona::where('id_usuario','=',null)->where('id_tipo_persona','=',1)//Trabajador
+    {
+
+        try
+        {
+            $perfiles=Perfil::all();
+            $personas = Persona::where('id_usuario','=',null)->where('id_tipo_persona','=',1)//Trabajador
                          ->orwhere('id_usuario','=',null)->where('id_tipo_persona','=',2)//Postulante
                         ->get();
-        return view('usuario.create',compact('perfiles','personas'));
+            return view('usuario.create',compact('perfiles','personas'));           
+        }
+        catch(\Exception $e)
+        {
+            Log::error($e);
+            $error = 'UsuarioController-create';
+            return view('errors.corrigeme', compact('error'));            
+        }        
+
     }
 
     /**
@@ -113,115 +147,150 @@ class UsuarioController extends Controller
      */
     public function store(UserCreateRequest $request)
     {
-        $input = $request->all();
-        if($input['perfil_id']!=-1){
-            DB::beginTransaction();
-            try{
-                $user = new User();
-                $user->name = $input['name'];
-                $user->email = $input['email'];
-                /*$user->password = bcrypt($input['password']);*/
-                $user->password = $input['password'];
-                $user->perfil_id = $input['perfil_id'];
-                $user->save();
-                /*dd($input['persona_id']);*/
-                $persona=Persona::find($input['persona_id']);
-                $persona->id_usuario=$user->id;
-                $persona->save();
-                Session::flash('message','Nuevo Usuario Asignado Correctamente');
+
+        try
+        {
+            $input = $request->all();
+            if($input['perfil_id']!=-1){
+                DB::beginTransaction();
+                try{
+                    $user = new User();
+                    $user->name = $input['name'];
+                    $user->email = $input['email'];
+                    /*$user->password = bcrypt($input['password']);*/
+                    $user->password = $input['password'];
+                    $user->perfil_id = $input['perfil_id'];
+                    $user->save();
+                    /*dd($input['persona_id']);*/
+                    $persona=Persona::find($input['persona_id']);
+                    $persona->id_usuario=$user->id;
+                    $persona->save();
+                    Session::flash('message','Nuevo Usuario Asignado Correctamente');
+                }
+                catch(ValidateException $e){
+                    DB::rollback();
+                    var_dump($e->getErrors());
+                }
+                DB::commit();
+                return Redirect::to('/usuario');
             }
-            catch(ValidateException $e){
-                DB::rollback();
-                var_dump($e->getErrors());
-            }
-            DB::commit();
-            return Redirect::to('/usuario');
+            else{
+                $perfiles=Perfil::all();
+                $personas = Persona::where('id_usuario','=',null)->where('id_tipo_persona','=',1)//Trabajador
+                                 ->orwhere('id_usuario','=',null)->where('id_tipo_persona','=',2)//Postulante
+                                 ->get();
+                Session::flash('message-error','Seleccione un perfil para el usuario');
+                return view('usuario.create',compact('perfiles','personas'));
+            }           
         }
-        else{
-            $perfiles=Perfil::all();
-            $personas = Persona::where('id_usuario','=',null)->where('id_tipo_persona','=',1)//Trabajador
-                             ->orwhere('id_usuario','=',null)->where('id_tipo_persona','=',2)//Postulante
-                             ->get();
-            Session::flash('message-error','Seleccione un perfil para el usuario');
-            return view('usuario.create',compact('perfiles','personas'));
-        }
+        catch(\Exception $e)
+        {
+            Log::error($e);
+            $error = 'UsuarioController-store';
+            return view('errors.corrigeme', compact('error'));            
+        }        
+
         
     }
-    public function changepassword(){
-        $perfil = 'socio';
-        switch (\Auth::user()->perfil_id) {
-                case '1':
-                    $perfil='socio';
-                    break;
-                case '2':
-                    $perfil='admin';
-                    break;
-                case '3':
-                    $perfil='admin-pagos';
-                    break;
-                case '4':
-                    $perfil='admin-registros';
-                    break;
-                case '5':
-                    $perfil='gerente';
-                    break;
-                case '6':
-                    $perfil='admin-persona';
-                    break;
-                case '7':
-                    $perfil='admin-reserva';
-                    break;
-                case '8':
-                    $perfil='control-ingresos';
-                    break;
-        }
+    public function changepassword()
+    {
 
-       return view('auth.changepassword', compact('perfil'));
+
+        try
+        {
+            $perfil = 'socio';
+            switch (\Auth::user()->perfil_id) {
+                    case '1':
+                        $perfil='socio';
+                        break;
+                    case '2':
+                        $perfil='admin';
+                        break;
+                    case '3':
+                        $perfil='admin-pagos';
+                        break;
+                    case '4':
+                        $perfil='admin-registros';
+                        break;
+                    case '5':
+                        $perfil='gerente';
+                        break;
+                    case '6':
+                        $perfil='admin-persona';
+                        break;
+                    case '7':
+                        $perfil='admin-reserva';
+                        break;
+                    case '8':
+                        $perfil='control-ingresos';
+                        break;
+            }
+
+           return view('auth.changepassword', compact('perfil'));          
+        }
+        catch(\Exception $e)
+        {
+            Log::error($e);
+            $error = 'UsuarioController-changepassword';
+            return view('errors.corrigeme', compact('error'));            
+        }        
+
     }
 
     public function confirmchangepassword(ChangePasswordRequest $request)
     {
-  
-        /*if(Auth::attempt(['password'=>$request['password_current']]))*/
-        if(Hash::check($request['password_current'],Auth::user()->password))
+
+        try
         {
-            $user=new User;
-            $user->where('email','=',Auth::user()->email)
-                 ->update(['password' => bcrypt($request['password'])]);
-            $perfil = 'socio';
-            switch (\Auth::user()->perfil_id) {
-                case '1':
-                    $perfil='/socio';
-                    break;
-                case '2':
-                    $perfil='/admin-general';
-                    break;
-                case '3':
-                    $perfil='/admin-pagos';
-                    break;
-                case '4':
-                    $perfil='/admin-registros';
-                    break;
-                case '5':
-                    $perfil='/gerente';
-                    break;
-                case '6':
-                    $perfil='/admin-persona';
-                    break;
-                case '7':
-                    $perfil='/admin-reserva';
-                    break;
-                case '8':
-                    $perfil='/control-ingresos';
-                    break;
+            /*if(Auth::attempt(['password'=>$request['password_current']]))*/
+            if(Hash::check($request['password_current'],Auth::user()->password))
+            {
+                $user=new User;
+                $user->where('email','=',Auth::user()->email)
+                     ->update(['password' => bcrypt($request['password'])]);
+                $perfil = 'socio';
+                switch (\Auth::user()->perfil_id) {
+                    case '1':
+                        $perfil='/socio';
+                        break;
+                    case '2':
+                        $perfil='/admin-general';
+                        break;
+                    case '3':
+                        $perfil='/admin-pagos';
+                        break;
+                    case '4':
+                        $perfil='/admin-registros';
+                        break;
+                    case '5':
+                        $perfil='/gerente';
+                        break;
+                    case '6':
+                        $perfil='/admin-persona';
+                        break;
+                    case '7':
+                        $perfil='/admin-reserva';
+                        break;
+                    case '8':
+                        $perfil='/control-ingresos';
+                        break;
+                }
+                Session::flash('message','Su contraseña ha sido cambiada con éxito');
+                return Redirect::to($perfil)->with('message','Su contraseña ha sido cambiada con éxito');
             }
-            Session::flash('message','Su contraseña ha sido cambiada con éxito');
-            return Redirect::to($perfil)->with('message','Su contraseña ha sido cambiada con éxito');
+            else{
+                Session::flash('message-error','Contraseña incorrecta');
+                return Redirect::to('/password/change');
+            }        
         }
-        else{
-            Session::flash('message-error','Contraseña incorrecta');
-            return Redirect::to('/password/change');
-        }
+        catch(\Exception $e)
+        {
+            Log::error($e);
+            $error = 'UsuarioController-confirmchangepassword';
+            return view('errors.corrigeme', compact('error'));            
+        }        
+  
+
         
     }
 
