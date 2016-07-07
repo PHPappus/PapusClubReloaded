de868db015e2a5a9f94b346bfa915aad233987d4
Victor Fuentes Ramos <Victor Fuentes Ramos>
2016-07-07 07:15:16 -0500

    try catch

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -12,29 +12,35 @@ class EmailController extends Controller
 {
     public function send(Request $request)
     {
-    	$title =$request->input('title');
-    	$content = $request->input('content');
-    	$subject ='Registro de usuario';
-    	$to ='juan0094@gmail.com';
+        try{
+        	$title =$request->input('title');
+        	$content = $request->input('content');
+        	$subject ='Registro de usuario';
+        	$to ='juan0094@gmail.com';
 
-        Mail::send('emails.send', ['title' => $title, 'content' => $content], function ($message) use($subject,$to)
+            Mail::send('emails.send', ['title' => $title, 'content' => $content], function ($message) use($subject,$to)
+            {
+
+                $message->from('registros@papusclub.com', 'Juan Ignacio Ferraro');
+                $message->to($to);
+    			$message->subject($subject);
+
+    			//$message->sender($address, $name = null);
+    			//$message->to($address, $name = null);
+    			//$message->cc($address, $name = null);
+    			//$message->bcc($address, $name = null);
+    			//$message->replyTo($address, $name = null);
+
+    			//$message->priority($level);
+    			//$message->attach($pathToFile, array $options = []);            
+
+            });
+
+            return response()->json(['message' => 'Request completed']);     
+        }catch (\Exception $e)
         {
-
-            $message->from('registros@papusclub.com', 'Juan Ignacio Ferraro');
-            $message->to($to);
-			$message->subject($subject);
-
-			//$message->sender($address, $name = null);
-			//$message->to($address, $name = null);
-			//$message->cc($address, $name = null);
-			//$message->bcc($address, $name = null);
-			//$message->replyTo($address, $name = null);
-
-			//$message->priority($level);
-			//$message->attach($pathToFile, array $options = []);            
-
-        });
-
-        return response()->json(['message' => 'Request completed']);       
+            $error = 'EmailController';
+            return view('errors.corrigeme', compact('error'));
+        }  
     }
 }
