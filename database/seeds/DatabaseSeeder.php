dfbb48e52f9bca33bdba8ce6da1a5b91c9171f69
maxvg <christian.vilcapomag@pucp.pe>
2016-07-07 05:58:43 -0500

    cambios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -42,8 +42,8 @@ class DatabaseSeeder extends Seeder
         //$this->call(TarifaFamiliarTableSeeder::class);
         //$this->call(TarifaActividadTableSeeder::class);
 
-        $this->call(FamiliarxPostulanteTableSeeder::class);
-        $this->call(ConcesionariaTableSeeder::class);
+        //$this->call(FamiliarxPostulanteTableSeeder::class);
+        //$this->call(ConcesionariaTableSeeder::class);
 
         //$this->call(TarifaTallerTableSeeder::class);
         $this->call(TrabajadorTableSeeder::class);
